3f6c193a1ae2df3a0d85350fb9949057208e2904
Du Spada <du.spada@gmail.com>
2019-09-24 00:04:59 -0300

    feat(dependencies): Added some dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-console */
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import '~/config/ReactotronConfig';
-
-import store from './store';
-
+import './config/Text'; // Global Text Config
+import './config/ReactotronConfig';
+import './config/StatusBarConfig';
 import Routes from '~/routes';
+import store from './store';
 
 const App = () => (
   <Provider store={store}>
@@ -13,4 +14,6 @@ const App = () => (
   </Provider>
 );
 
-export default App;
+const TheApp = __DEV__ ? console.tron.overlay(App) : App;
+
+export default TheApp;
