c00c944595b72c7c3fc90996562319dbec34bbac
Du Spada <du.spada@gmail.com>
2019-09-25 00:22:17 -0300

    feat(dependencies): Added Keychain

--- android/app/src/main/java/com/templatern59/MainApplication.java
+++ android/app/src/main/java/com/templatern59/MainApplication.java
@@ -3,6 +3,7 @@ package com.templatern59;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.keychain.KeychainPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new KeychainPackage(),
           new RNCameraPackage(),
           new LinearGradientPackage(),
           new VectorIconsPackage(),
